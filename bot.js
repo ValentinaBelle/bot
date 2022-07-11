8e3680f165b76ce9677dd7a331d0369e580f43c4
Valentina Zavodova <106472523+ValentinaBelle@users.noreply.github.com>
2022-07-11 10:30:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,88 @@
-let keywords = ["things to do in miami south beach", "places to go in south beach florida", "attractions miami florida"];
-let keyword = keywords[getRandom(0, keywords.length)];
-let links = document.links;
-let googleInput = document.getElementsByName("q")[0];
-let btnK = document.getElementsByName("btnK")[0];
+// ==UserScript==
+// @name         Bing Bot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Hiya!
+// @author       Valentina
+// @match        https://www.bing.com/*
+// @match        https://www.audemarspiguet.com/*
+// @match        https://www.britannica.com/*
+// @match        https://ocr.ussailing.org/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+let sites = {
+  "audemarspiguet.com":["Audemars Piguet the royal oak", "Audemars Piguet & Golf"],
+  "britannica.com":["Biography of Edgar Dega", "Edgar Dega colour and line"],
+  "ocr.ussailing.org":["US Open Sailing Series", "US Open sailing series long beach", "US Open Sailing Series Miami results"]
+}
 
-if (btnK !== undefined) {
-googleInput.value = keyword;
+let site = Object.keys(sites)[getRandom(0, Object.keys(sites).length)];
+let keywords = sites[site];
+let keyword = keywords[getRandom(0, keywords.length)];
+
+let links = document.links;
+let bingInput = document.getElementById("sb_form_q");
+let button = document.getElementById("sb_form_go");
+
+if (button !== undefined) {
+  document.cookie = `site=${site}`;
+}else if (location.hostname == "www.bing.com"){
+  site = getCookie("site");
 } else {
-for (let i = 0; i < links.length; i++) {
-    if (links[i].href.includes("visitflorida.com")) {
-    let link = links[i];
-    console.log("find the site " + links[i]);
-    link.click();
-    break;
+  site = location.hostname;
+}
+
+
+if (button !== undefined) {
+  let i = 0;
+  let timerId = setInterval(() => {
+    bingInput.value += keyword[i];
+    i++;
+    if (i == keyword.length) {
+      clearInterval(timerId);
+      button.click();
     }
+  }, 700);
+} else if (location.hostname == site) {
+  setInterval(() => {
+    let index = getRandom(0, links.length);
+
+    if (getRandom(0, 101) >= 70) {
+      location.href = "https://www.bing.com/";
+    } else if (links[index].href.indexOf(site) !== -1){
+      links[index].click();}
+  }, getRandom(2000, 5000));
+} else {
+  let nextBingPage = true;
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].href.includes(site)) {
+      let link = links[i];
+      nextBingPage = false;
+      console.log("Нашел строку " + links[i]);
+      setTimeout(() => {
+        link.click();
+      }, getRandom(2000, 5000));
+
+      break;
+    }
+  }
+  if (document.querySelector("sb_pagS").innerText == "5") {
+    nextBingPage = false;
+    location.href = "https://www.bing.com/";
+  }
+  if (nextBingPage) {
+    setTimeout(() => {
+      pnnext.click();
+    }, getRandom(3000, 7000));
+  }
 }
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
-function getRandom (min, max) {
-return Math.floor(Math.random()*(max - min) + min);
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
